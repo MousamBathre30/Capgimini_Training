39ae81a0764313a261877bdebea0b4c8e3c61705
Mousam bathre <bathremousam@gmail.com>
2025-01-06 19:33:15 +0530

    Integrate all functionallities in Calculator

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,5 +1,9 @@
 public class Main{
     public static void main (String args[]){
         System.out.println("Welcom to the Calculator!");
+        System.out.println("Addition: " + Addition.add(5, 3));
+        System.out.println("Subtraction: " + Subtraction.subtract(5, 3));
+        System.out.println("Multiplication: " + Multiplication.multiply(5, 3));
+        System.out.println("Division: " + Division.divide(5, 3));
     }
 }
